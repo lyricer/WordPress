28fa99e9223c5cc778a1c65878557b5b4ccd3390
lyricn <missyou@lyricn.com>
2023-10-29 11:08:10 +0800

    add birthday html
    新增birthday頁面

---
+++ README.md
@@ -0,0 +1,11 @@
+# WordPress
+
+## 基於WordPress配使用
+
+### 生日快樂
+
+birthday
+
+demo : https://54jojo.com/birthday/
+
+
---
+++ birthday/time.js
@@ -0,0 +1,23 @@
+function calculateTime() {
+    var birthDate = new Date("12/08/1995 00:00:00"); // 修改出生日期
+    var today = new Date();
+    var timeDifference = today.getTime() - birthDate.getTime();
+    
+    var msPerDay = 24 * 60 * 60 * 1000;
+    var daysDifference = Math.floor(timeDifference / msPerDay);
+    var remainingTime = timeDifference % msPerDay;
+    
+    var msPerHour = 60 * 60 * 1000;
+    var hoursDifference = Math.floor(remainingTime / msPerHour);
+    remainingTime %= msPerHour;
+    
+    var msPerMinute = 60 * 1000;
+    var minutesDifference = Math.floor(remainingTime / msPerMinute);
+    remainingTime %= msPerMinute;
+    
+    var secondsDifference = Math.floor(remainingTime / 1000);
+    
+    span_dt_dt.innerHTML = daysDifference + "天" + hoursDifference + "小时" + minutesDifference + "分" + secondsDifference + "秒";
+}
+
+calculateTime();
